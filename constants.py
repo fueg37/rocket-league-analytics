018523134189b4b3b222ebf89a84a5a94fb53aa5
fueg37 <owensr37@gmail.com>
2026-02-16 16:03:41 -0500

    Revert "Revert "Revert "Introduce canonical goal geometry contract and remove hardcoded goal-mouth literals"""

--- constants.py
+++ constants.py
@@ -16,14 +16,10 @@ MAX_STORED_MATCHES = 12
 FIELD_HALF_X = 4096    # Sideline (half-width)
 FIELD_HALF_Y = 5120    # End line (half-length)
 WALL_HEIGHT = 2044
-GOAL_HALF_WIDTH = 1784  # Goal opening half-width
-GOAL_WIDTH = GOAL_HALF_WIDTH * 2
+GOAL_HALF_W = 1784     # Goal opening half-width
 GOAL_DEPTH = 880       # Goal depth behind end line
 GOAL_HEIGHT = 642
 CENTER_CIRCLE_R = 1140
-
-# Backward-compatible alias (prefer GOAL_HALF_WIDTH for new code)
-GOAL_HALF_W = GOAL_HALF_WIDTH
 
 # Axis padding beyond field walls (room for goal depth + labels)
 AXIS_PAD_X = 600
