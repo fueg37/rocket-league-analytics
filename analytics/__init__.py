5205281987784a1499f3938ffe7ebed87f278d66
fueg37 <owensr37@gmail.com>
2026-02-16 21:44:51 -0500

    Fix session-state key conflict and legacy/CI compatibility regressions

--- analytics/__init__.py
+++ analytics/__init__.py
@@ -1,1 +1,22 @@
 """Analytics helpers package."""
+
+from analytics.contracts import ScalarMetricContract, flatten_metric_contract, metric_contract, read_metric_contract
+from analytics.stats_uncertainty import (
+    DEFAULT_UNCERTAINTY_SEED,
+    bayesian_binomial_interval,
+    bootstrap_mean_interval,
+    deterministic_seed,
+    reliability_from_sample_size,
+)
+
+__all__ = [
+    "ScalarMetricContract",
+    "metric_contract",
+    "flatten_metric_contract",
+    "read_metric_contract",
+    "DEFAULT_UNCERTAINTY_SEED",
+    "deterministic_seed",
+    "bootstrap_mean_interval",
+    "bayesian_binomial_interval",
+    "reliability_from_sample_size",
+]
